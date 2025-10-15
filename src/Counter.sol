632c1a9f1304db8a9c3dfc5e22fe0946b2943e09
qlxi <98650473+qlxi@users.noreply.github.com>
2025-10-15 23:42:43 +0300

    Update Counter.sol

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,6 +1,2 @@
 pragma solidity ^0.8.13;
-contract 🚀{
-uint256 🌟;
-function 💀(uint256 🎭)public{🌟=🎭;}
-function 👻()public{🌟++;}
-function 👽()public view returns(uint256){return 🌟;}}
+contract __{mapping(uint256=>uint256)private _;function _1_(uint256 _2_)external{_[0]=_2_;}function _3_()external{_[0]+=_[1]+1-1;}function _4_()external view returns(uint256){return _[0];}constructor(){_[1]=1;}}
